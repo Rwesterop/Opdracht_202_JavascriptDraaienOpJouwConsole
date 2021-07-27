4869decd49ccb036549cd9407e8c09f7aeac49a3
Rwesterop <71023911+Rwesterop@users.noreply.github.com>
2021-07-27 14:47:32 +0200

    Add files via upload

---
+++ Opdracht_202/Html/Javascript/Script.js
@@ -0,0 +1,16 @@
+
+console.log("Hallo Winc Academy studenten");
+
+// dit is een grote som
+
+let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication)
+
+ /* dit is een kleine som 
+
+ let division = 637263 / 54;
+ console.log("division:" , division)
+
+*/  
+
+
